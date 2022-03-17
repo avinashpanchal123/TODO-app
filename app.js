084c6dc4bfa5c74d6ae984f4ff86be6161d7fc85
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-18 02:13:07 +0530

    getting form data inside app.js console
    imported ejs and created form for making list of newtasks getting newtask in app.js console

--- app.js
+++ app.js
@@ -3,11 +3,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/index.html")
+let tasks = []
+
+
+app.set("view engine", "ejs");
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const d = new Date();
+let month = months[d.getMonth()];
+let day = days[d.getDay()];
+let date = d.getDate()
+let output = [month, day, date].join(" ")
+
+
+console.log(output);
+
+
+
+app.get("/", (req, res) => {
+ 
+  res.render("list", {kindOfDay : output});
+});
+
+app.post("/", (req, res)=>{
+    console.log(req.body.newTask);
+  
 })
 
-app.listen(2255, ()=>{
-    console.log("listening on port 2255");
-})
+app.listen(2255, () => {
+  console.log("listening on port 2255");
+});
+
+
